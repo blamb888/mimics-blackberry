5d786d379f3e558c3d776627ff1488f6597857f8
Andrew Wallace <andrew.wallace.english@gmail.com>
2021-03-01 18:04:07 +0900

    added dependent destroy to events in calendar.rb

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,7 +1,7 @@
 class Calendar < ApplicationRecord
   belongs_to :user
   has_many :users, through: :user_calendars
-  has_many :events
+  has_many :events, dependent: :destroy
 
   validates :start_day, presence: true
   validates :start_year, presence: true
