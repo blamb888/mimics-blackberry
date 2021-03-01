0e5dc1b9cb749f27bb26ac748b47c7d6115d678d
Brandon Lamb <blamb888@gmail.com>
2021-03-01 17:32:31 +0900

    added pundit gem and created controllers

---
+++ app/policies/calendar_policy.rb
@@ -0,0 +1,12 @@
+class CalendarPolicy < ApplicationPolicy
+  class Scope < Scope
+
+    def create?
+      return true
+    end
+
+    def resolve
+      scope.all
+    end
+  end
+end
---
+++ app/policies/event_policy.rb
@@ -0,0 +1,12 @@
+class EventPolicy < ApplicationPolicy
+  class Scope < Scope
+
+    def create?
+      return true
+    end
+
+    def resolve
+      scope.all
+    end
+  end
+end
