0ec34e37d90914317d9e908eabdd94c1538b70a5
Brandon Lamb <blamb888@gmail.com>
2021-03-02 12:41:15 +0900

    add some changes

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,17 +1,20 @@
 class CalendarsController < ApplicationController
-  before_action :find_calendar, only: [:show]
+  # before_action :find_calendar, only: [:show]
 
   def show
-    authorize @calendar
+    @calendar = Calendar.first
+    # authorize @calendar
   end
 
 
   private
 
-  def find_calendar
-    @calendar = Calendar.find(params[:id])
+  # def find_calendar
+  #   @calendar = Calendar.find(params[:id])
+  # end
 
   def calendar_params
     params.require(:calendar).permit(:start_day, :start_year, :current_day, :months, :weekdays, :user)
+  end
 
 end
