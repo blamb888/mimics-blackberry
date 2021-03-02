e9599ad0395ebeb616e15159875b1b35879efdae
Brandon Lamb <blamb888@gmail.com>
2021-03-02 14:37:55 +0900

    display days of week in months divs

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,16 +1,17 @@
 class CalendarsController < ApplicationController
-  # before_action :find_calendar, only: [:show]
+  before_action :find_calendar, only: [:show]
 
   def show
-    @calendar = Calendar.first
+    @calendar = Calendar.find(params[:id])
+    # authorize @calendar
   end
 
 
   private
 
-  # def find_calendar
-  #   @calendar = Calendar.find(params[:id])
-  # end
+  def find_calendar
+    @calendar = Calendar.find(params[:id])
+  end
 
   def calendar_params
     params.require(:calendar).permit(:start_day, :start_year, :current_day, :months, :weekdays, :user)
