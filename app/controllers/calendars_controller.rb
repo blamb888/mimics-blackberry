68fcff016605849bd5ce6df30fc9fae6af89da1b
Brandon Lamb <blamb888@gmail.com>
2021-03-02 13:23:12 +0900

    got divs to show for weekdays

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,16 +1,16 @@
 class CalendarsController < ApplicationController
-  before_action :find_calendar, only: [:show]
+  # before_action :find_calendar, only: [:show]
 
   def show
-    authorize @calendar
+    @calendar = Calendar.first
   end
 
 
   private
 
-  def find_calendar
-    @calendar = Calendar.find(params[:id])
-  end
+  # def find_calendar
+  #   @calendar = Calendar.find(params[:id])
+  # end
 
   def calendar_params
     params.require(:calendar).permit(:start_day, :start_year, :current_day, :months, :weekdays, :user)
