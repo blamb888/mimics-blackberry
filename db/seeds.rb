22ec3fbb8ae23fe067e2400b126f7ff24bcdb31e
Brandon Lamb <blamb888@gmail.com>
2021-03-02 14:45:33 +0900

    set up Ebberon calendar seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,19 @@ u.save
 
 puts("USER CREATED")
 
+# calendar = Calendar.new({
+#   start_day: 1,
+#   start_year: 1,
+#   current_day: 1,
+#   months: {january:31},
+#   weekdays:{monday:0, tuesday: 1, wednesday: 2, thursday: 3, friday: 4, saturday: 5, sunday: 6}
+# })
 calendar = Calendar.new({
   start_day: 1,
   start_year: 1,
   current_day: 1,
-  months: {january:31},
-  weekdays:{monday:0, tuesday: 1, wednesday: 2, thursday: 3, friday: 4, saturday: 5, sunday: 6}
+  months: {Zarantyr: 28, Olarune: 28, Therendor: 28, Eyre: 28, Dravago: 28, Nymm: 28, Lharvion: 28, Barrakas: 28, Rhaan: 28, Sypheros: 28, Aryth: 28, Vult: 28 },
+  weekdays:{Sul: 0, Mol: 1, Zol: 2, Wir: 3, Zor: 4, Far: 5, Sar: 6}
 })
 calendar.user = u
 calendar.save
