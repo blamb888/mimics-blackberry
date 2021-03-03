773d50445da7c81464535bbab9b47f6e7e0d8695
Brandon Lamb <blamb888@gmail.com>
2021-03-03 13:08:47 +0900

    added find_calendar method to events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,8 @@
 class EventsController < ApplicationController
+  before_action :find_calendar, only: [:new, :create]
 
   def new
     @event = Event.new
-    @calendar = Calendar.find(params[:calendar_id])
     authorize @calendar
     authorize @event
   end
@@ -15,11 +15,15 @@ class EventsController < ApplicationController
     if @event.save
       redirect_to calendar_path(@calendar)
     else
-      render :calendar
+      redirect_to calendar_path(@calendar)
     end
   end
 
   private
+
+  def find_calendar
+    @calendar = Calendar.find(params[:calendar_id])
+  end
 
   def event_params
     params.require(:event).permit(:name, :category, :day_number, :description, :user)
