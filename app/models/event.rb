ca9981a2ca0549f937376e76304dfbab0927e889
Brandon Lamb <blamb888@gmail.com>
2021-03-03 12:36:48 +0900

    add collection to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ApplicationRecord
   belongs_to :calendar
   belongs_to :user
 
+  CATEGORIES = ['Weather', 'Notes', 'Holiday', 'Item', 'Character']
+
   validates :name, presence: true
   validates :category, presence: true
   validates :day_number, presence: true
