0e86e3b1d3042e6a4a24b957f23440dafcee21f2
Andrew Wallace <andrew.wallace.english@gmail.com>
2021-03-03 09:43:09 +0900

    divided months with h1 titles

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_070106) do
-
+ActiveRecord::Schema.define(version: 2021_03_01_090000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +21,6 @@ ActiveRecord::Schema.define(version: 2021_03_01_070106) do
     t.integer "current_day"
     t.json "months"
     t.json "weekdays"
-
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
