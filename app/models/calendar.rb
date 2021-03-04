b05576f4403c8b55fa98938dfeeca8649c46285f
Albert Ferre <libraescorpio1982@gmail.com>
2021-03-04 15:33:59 +0900

    Add back, next, previous buttons plus back/next month funcntionality

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -7,4 +7,20 @@ class Calendar < ApplicationRecord
   validates :start_year, presence: true
   validates :months, presence: true
   validates :weekdays, presence: true
+
+  def get_next_month(month_index)
+    next_month = month_index.to_i + 1
+    if next_month > self.months.length - 1
+      next_month = 0
+    end
+    return next_month
+  end
+
+  def get_previous_month(month_index)
+    next_month = month_index.to_i - 1
+    if next_month < 0
+      next_month = self.months.length - 1
+    end
+    return next_month
+  end
 end
