54e7c2fa16481ab80d6095eafb7d7de82f2b56ab
Brandon Lamb <blamb888@gmail.com>
2021-03-04 12:01:55 +0900

    put breaks back in calculate_date method

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -73,35 +73,35 @@ class CalendarsController < ApplicationController
     params.require(:calendar).permit(:start_day, :start_year, :current_day, :months, :weekdays, :user)
   end
 
-  def calculate_date(target_day)
-    @calendar = Calendar.first
-    @sum_of_months = 0
-    @target_day = target_day
-    @current_month = 0
-    @current_day = 0
-    #iterate over the months has
-    @calendar.months.values.each.with_index do |month, index|
-      if(@sum_of_months + month > @target_day)
-        #day is in this month set current month to index
-        puts "breaking"
-        @current_month = index;
-        break
-      else
-        # else add number of days to sum
-        puts "adding days"
-        @sum_of_months += month
-      end
-    end
-    #use current month to get day of the month
-    @calendar.months.values[@current_month].times do
-      @sum_of_months += 1
-      @current_day += 1
-      #if the sum is the same as the target day
-      if(@sum_of_months == @target_day)
-        break
-      end
-    end
-    @final_date = { @calendar.months.keys[@current_month] => @current_day}
-    return @final_date
-  end
+  # def calculate_date(target_day)
+  #   @calendar = Calendar.first
+  #   @sum_of_months = 0
+  #   @target_day = target_day
+  #   @current_month = 0
+  #   @current_day = 0
+  #   #iterate over the months has
+  #   @calendar.months.values.each.with_index do |month, index|
+  #     if(@sum_of_months + month > @target_day)
+  #       #day is in this month set current month to index
+  #       puts "breaking"
+  #       @current_month = index;
+  #       break
+  #     else
+  #       # else add number of days to sum
+  #       puts "adding days"
+  #       @sum_of_months += month
+  #     end
+  #   end
+  #   #use current month to get day of the month
+  #   @calendar.months.values[@current_month].times do
+  #     @sum_of_months += 1
+  #     @current_day += 1
+  #     #if the sum is the same as the target day
+  #     if(@sum_of_months == @target_day)
+  #       break
+  #     end
+  #   end
+  #   @final_date = { @calendar.months.keys[@current_month] => @current_day}
+  #   return @final_date
+  # end
 end
