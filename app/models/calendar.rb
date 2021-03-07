1d1470b3f308c78569c0cd5b7f04d6ba64600605
Brandon Lamb <blamb888@gmail.com>
2021-03-07 21:11:42 +0900

    add create! method to template creation in the calendar model to solve for null methods and validation errors. TODO still need to fetch current_user somehow in the model method

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -51,7 +51,8 @@ def calculate_date(target_day) #TODO maybe I could refactor this.
   def self.new_template(template)
     case template
     when "eberron"
-      Calendar.new(
+      calendar = Calendar.create!(
+        user: User.first,
         name: "Eberron",
         start_day: 1,
         start_year: 1,
@@ -59,9 +60,24 @@ def calculate_date(target_day) #TODO maybe I could refactor this.
         months: {Zarantyr: 28, Olarune: 28, Therendor: 28, Eyre: 28, Dravago: 28, Nymm: 28, Lharvion: 28, Barrakas: 28, Rhaan: 28, Sypheros: 28, Aryth: 28, Vult: 28 },
         weekdays:{Sul: 0, Mol: 1, Zol: 2, Wir: 3, Zor: 4, Far: 5, Sar: 6}
       )
-      Event.create!(
-        calendar: self.last,
-        user: Calendar.last.user,
+    end
+       Event.create!(
+        calendar: calendar,
+        user: User.first,
+        name: "rebirth eve",
+        category: "holiday",
+        day_number: 1,
+        rich_description: '<b>Rebirth Eve</b><br /><a href="https://static.wikia.nocookie.net/eberron/images/5/55/Silver-flame.jpg/revision/latest?cb=20090905064640" class="image"><img alt="Silver-flame.jpg" src="data:image/gif;base64,R0lGODlhAQABAIABAAAAAP///yH5BAEAAAEALAAAAAABAAEAQAICTAEAOw%3D%3D" decoding="async" width="20" height="22" data-image-name="Silver-flame.jpg" data-image-key="Silver-flame.jpg" data-src="https://static.wikia.nocookie.net/eberron/images/5/55/Silver-flame.jpg/revision/latest/scale-to-width-down/20?cb=20090905064640" class="lazyload" /></a> <a href="/wiki/The_Silver_Flame" class="mw-redirect" title="The Silver Flame">The Silver Flame</a><br />This flamic festival, which takes the form of a spiritual vigil, celebrates the winter solstice.<br />'
+      )
+      raise
+  end
+
+  def self.new_template_event(calendar)
+    case template
+    when "eberron"
+   Event.create!(
+        calendar: calendar,
+        user: calendar.user,
         name: "rebirth eve",
         category: "holiday",
         day_number: 1,
