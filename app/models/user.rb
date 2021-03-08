3b244cde9c35e75eb4bd7ed1cb4352e0483afd78
Andrew Wallace <andrew.wallace.english@gmail.com>
2021-03-08 18:24:52 +0900

    added notifications in drop down

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,23 @@ class User < ApplicationRecord
   
   has_many :calendars_as_owner, class_name: "Calendar", foreign_key: "user_id"
   has_many :events
-  has_many :notification
+  has_many :notifications
   has_many :user_calendars
   has_many :calendars, through: :user_calendars
   has_one_attached :photo
 
   def all_calendars
     calendars + calendars_as_owner
+  end
+
+  def all_notifications
+    n_array = []
+    notifications.each do |n|
+      n_string = format.html do
+        link_to "#{n.user.email} added you to #{n.target.name}", calendar_path(n.target)
+      end
+      n_array << n_string
+    end
+    return n_array
   end
 end
