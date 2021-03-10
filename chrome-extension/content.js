a1f5d7bb34b7214a0365a69e79be5f65222ec353
Brandon Lamb <blamb888@gmail.com>
2021-03-10 20:15:44 +0900

    extension works for current day notes

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -3,15 +3,18 @@ chrome.runtime.onMessage.addListener(
     // actions based on the request (which corresponds to the object we sent in our message)
     if (request === 'getWhatever') {
       // Get the stuff you want from the page `result`
-      const currentDay = document.querySelector(".current-day").innerText
+      const currentDay = document.querySelector(".current-day div").innerText
       const result = document.getElementById("calendar-container").dataset
-      console.log(result)
+      const dayValue = document.getElementById("chrome-variable").dataset
+      console.log(dayValue.currentDay)
       sendResponse({
+        currentDay: result.currentDay,
         calendarId: result.calendarId,
         userId: result.userId,
         calendarName: result.calendarName,
         monthName: result.monthName,
-        currentDay: currentDay
+        currentDay: currentDay,
+        dayValue: dayValue.currentDay
       })
     }
   }
--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -3,10 +3,11 @@ function listenClick() {
     console.log(tabs)
     chrome.tabs.sendMessage(tabs[0].id, 'getWhatever', (result) => {
       //popupDisplay
-      console.log(result)
+      console.log(result.currentDay);
       document.getElementById("calendar-name").innerText = result.calendarName
       document.getElementById("current-date").innerText =`${result.monthName}-${result.currentDay}`
       //preparing form data
+      const dayValue = result.dayValue
       const calendar_id = result.calendarId
       const user_id = result.userId
       console.log(result)
@@ -15,7 +16,7 @@ function listenClick() {
         event.preventDefault()
         console.log(document)
         const title = document.querySelector('#name').value;
-        const day_number = document.querySelector('#day_number').value;
+        // const day_number = document.querySelector('#day_number').value;
         const category = document.querySelector('#category').value;
         const private = document.querySelector('#private').value;
         const rich_description = document.querySelector('#rich_description').value;
@@ -29,7 +30,8 @@ function listenClick() {
             "event": {
               "user_id": `${user_id}`,
               "calendar_id": `${calendar_id}`,
-              "day_number": `${day_number}`,
+              // "day_number": `${day_number}`,
+              "day_number": `${dayValue}`,
               "private": `${private}`,
               "category": `${category}`,
               "name": `${title}`,
