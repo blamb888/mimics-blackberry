f478172759c7fc36b6c4d1f9925203b698c3d93e
Brandon Lamb <blamb888@gmail.com>
2021-03-10 13:29:38 +0900

    yann is a genius. it is known

--- chrome-extension/send-data.js
+++ chrome-extension/send-data.js
@@ -1,7 +1,7 @@
 // script/send-data.js
 function fetchData() {
-  const title = document.querySelector('title').innerText;
-  const url = window.location.href;
+  const title = document.querySelector('form').innerText;
+  const content = window.location.href;
 
   return {
     title: title,
@@ -10,13 +10,13 @@ function fetchData() {
 }
 
 function sendData(data) {
-  const url = 'https://mimics-blackberry.herokuapp.com/';
+  const url = 'https://mimics-blackberry.herokuapp.com/calendars/:calendar_id/events';
   fetch(url, {
     method: 'POST',
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      // "author": "Mimic's Blackberry chrome extension",
-      // "content": `I've found something cool: ${data.title} on ${data.url}`
+      "author": "Mimic's Blackberry chrome extension",
+      "content": `I've found something cool: ${data.title} on ${data.content}`
     })
   })
 }
