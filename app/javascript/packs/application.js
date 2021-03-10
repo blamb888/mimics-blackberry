9f8775613a665c3e53a28a28beb1bf7cf6dc9ade
Ruben Rubilar <rubenrubilarbustos@gmail.com>
2021-03-10 19:47:04 +0900

    added icons inside modal forms

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,10 +24,10 @@ require("trix")
 require("@rails/actiontext")
 import "controllers"
 // Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+import { toggleEvent } from '../components/event_tag';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  toggleEvent()
 });
 
